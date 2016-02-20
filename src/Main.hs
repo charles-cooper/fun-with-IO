64048e1cfd1ab6ec822f674e9e3402f3e65f1fa2
Charles Cooper <cooper.charles.m@gmail.com>
2016-02-20 18:33:42 +0000

    ioref example

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,20 @@
 module Main where
 
+import Data.IORef
+
+import Control.Concurrent
+
+ioref :: String -> IO ()
+ioref str = do
+  ref <- newIORef str
+  forkIO $ do
+    hello <- readIORef ref
+    writeIORef ref $ reverse str
+  -- what will this print!
+  readIORef ref >>= putStrLn
+  readIORef ref >>= putStrLn
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  ioref "Hello, world!"
+
